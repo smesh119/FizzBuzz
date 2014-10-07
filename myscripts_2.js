a1c73d24a5dc12015c02cebe18c6a3b5c6b6f678
smesh119 <smesh119@gmail.com>
2014-10-07 09:48:37 -0400

    Fixed formatting

--- myscripts_2.js
+++ myscripts_2.js
@@ -1,42 +1,40 @@
 $(document).ready(function() {
 
-	/* 1. INPUT SECTION on keydown*/
-	$(".query").keydown(function(e){
-		if(e.which == 13){
-			var input = $("input[name=inputvalue]").val();
-		
-		for(i=1; i<=input; i++) {
-				if (i%3 === 0 && i%5 === 0){
-            	$(".listitemsadded").append("<li>FizzBuzz</li>");
-            	}
-    			else if (i%3===0){
-            	$(".listitemsadded").append("<li>Fizz</li>");
-        		} 
-    			else if (i%5===0) {
-            	$(".listitemsadded").append("<li>Buzz</li>");
-       			}
-      			else { $(".listitemsadded").append("<li>" + input + "</li>");
-       		 	}
-       	}	 	
-	}	
-});
+    /* 1. INPUT SECTION on keydown*/
+    $(".query").keydown(function(e) {
+        if (e.which == 13) {
+            var input = $("input[name=inputvalue]").val();
 
-	/* 2. when user types into input, remove prompt text*/
-	$(".query").on("click", function(e){
+            for (i = 1; i <= input; i++) {
+                if (i % 3 === 0 && i % 5 === 0) {
+                    $(".listitemsadded").append("<li>FizzBuzz</li>");
+                } else if (i % 3 === 0) {
+                    $(".listitemsadded").append("<li>Fizz</li>");
+                } else if (i % 5 === 0) {
+                    $(".listitemsadded").append("<li>Buzz</li>");
+                } else {
+                    $(".listitemsadded").append("<li>" + input + "</li>");
+                }
+            }
+        }
+    });
+
+    /* 2. when user types into input, remove prompt text*/
+    $(".query").on("click", function(e) {
         $(".query").val("");
     });
 
     $("button").on("click", function(e) {
-    	$("li").remove();
+        $("li").remove();
     });
 
 
-	/* 3. When user puts new text in input and hits enter, on keyup remove their input*/
-    $(".query").keyup(function(e){
+    /* 3. When user puts new text in input and hits enter, on keyup remove their input*/
+    $(".query").keyup(function(e) {
         if (e.which == 13) {
-        	$(".query").val("")
+            $(".query").val("")
         };
     });
-   
+
 
 });
