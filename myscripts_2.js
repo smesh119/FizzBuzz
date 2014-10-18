7eddc664d0f683c31519ffeb2af8d879569ccacb
smesh119 <smesh119@gmail.com>
2014-10-18 14:25:17 -0400

    update

--- myscripts_2.js
+++ myscripts_2.js
@@ -22,6 +22,7 @@ $(document).ready(function() {
                     break
                 } else {
                     $(".listitemsadded").append("<li>" + i + "</li>");
+                }    
             }
         }
     });
